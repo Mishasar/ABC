aadedb6dc4d183a81ee8b1bff3540ad695f14b33
Mikhail Saraykin <saraykin1996@gmail.com>
2019-07-28 21:09:59 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,36 @@
-import $ from "jquery";
-import "slick-slider";
+import $ from 'jquery';
+import 'slick-slider';
+import 'select2/dist/js/select2.full.js';
 
-$('.js-leader-slider').slick({
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	prevArrow: "<a href=\"javascript:void(0);\" class='leader__arrow leader__arrow_prev'></a>",
-	nextArrow: "<a href=\"javascript:void(0);\" class='leader__arrow leader__arrow_next'></a>",
-	autoplay: true,
-	autoplaySpeed: 2000,
-	infinite: true,
-	dots: true,
-	dotsClass: "leader__dots",
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: true,
-				arrows: false
-			}
-		}
-	]
+$(document).ready(function() {
+
+	$('.js-leader-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<a href="javascript:void(0);" class=\'leader__arrow leader__arrow_prev\'></a>',
+		nextArrow: '<a href="javascript:void(0);" class=\'leader__arrow leader__arrow_next\'></a>',
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		dots: true,
+		dotsClass: 'leader__dots',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+				},
+			},
+		],
+	});
+
+	$('.js-select').select2({
+		theme: 'custom',
+		width: '100%',
+		minimumResultsForSearch: -1,
+		placeholder: "Время строительства"
+	});
 });
