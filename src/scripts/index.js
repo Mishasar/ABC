1076650e4362dbebe0acb1aa95c0ae5851375553
Mikhail Saraykin <saraykin1996@gmail.com>
2019-07-31 23:03:16 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -27,10 +27,48 @@ $(document).ready(function() {
 		],
 	});
 
+	$('.js-spec-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		prevArrow: '<a href="javascript:void(0);" class=\'spec__arrow spec__arrow_prev\'></a>',
+		nextArrow: '<a href="javascript:void(0);" class=\'spec__arrow spec__arrow_next\'></a>',
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		dots: true,
+		dotsClass: 'spec__dots',
+		responsive: [
+			{
+				breakpoint: 960,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: false,
+				},
+			},
+		],
+	});
+
 	$('.js-select').select2({
 		theme: 'custom',
 		width: '100%',
 		minimumResultsForSearch: -1,
 		placeholder: "Время строительства"
+	});
+
+	$('.js-faq-title').on('click', function(e) {
+		const $el = $(e.target);
+		$el.toggleClass('faq__title_opened');
+		$el.siblings('.js-faq-inner').slideToggle();
 	});
 });
